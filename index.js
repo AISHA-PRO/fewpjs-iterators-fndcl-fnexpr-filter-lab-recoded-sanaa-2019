2db5760932079ea114d7e30d2d3dd7b7891c00b9
Aisha Rashed <aisha1rashed@gmail.com>
2019-11-22 09:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function fuzzyMatch (drivers, str){
    return e[0].toUpperCase() == str[0].toUpperCase(); });
    return newDriver;
 }
+
+function fuzzyMatch (drivers, str){
+  const newDriver = drivers.filter(e=>{
+   return e[0].toUpperCase() == str[0].toUpperCase(); });
+   return newDriver;
+}
