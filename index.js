d8f196d9ce3772f37af87cecf3abfaa6b7ff6903
Aisha Rashed <aisha1rashed@gmail.com>
2019-11-22 09:31:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function fuzzyMatch (drivers, str){
    return newDriver;
 }
 
-function fuzzyMatch (drivers, str){
+function matchName  (drivers, str){
   const newDriver = drivers.filter(e=>{
    return e[0].toUpperCase() == str[0].toUpperCase(); });
    return newDriver;
